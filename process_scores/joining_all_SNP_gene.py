bf78b5f6ad1c3f94084468c51aab810a3a034136
Trisha Karani <77947159+tkarani1@users.noreply.github.com>
2025-11-03 14:11:55 -0500

    Add files via upload

--- process_scores/joining_all_SNP_gene.py
+++ process_scores/joining_all_SNP_gene.py
@@ -106,65 +106,65 @@ no_transcript_tables = {
 
 ## 0. Make grouped Linker table
     # just need to run this the first time
-# vsm_ht = hl.read_table('gs://trisha-tmp/new_VSM_temp/vsm_base_per_gene_SNP_2.ht')
-# vsm_ht = vsm_ht.key_by('locus', 'alleles', 'ensg')
-# vsm_ht = vsm_ht.checkpoint('gs://trisha-tmp/new_VSM_temp/vsm_base_em_gene_k_lag.ht', overwrite=True)
-# vsm_ht = hl.read_table('gs://trisha-tmp/new_VSM_temp/vsm_base_em_gene_k_lag.ht')
+vsm_ht = hl.read_table('gs://trisha-tmp/new_VSM_temp/vsm_base_per_gene_SNP_2.ht')
+vsm_ht = vsm_ht.key_by('locus', 'alleles', 'ensg')
+vsm_ht = vsm_ht.checkpoint('gs://trisha-tmp/new_VSM_temp/vsm_base_em_gene_k_lag.ht', overwrite=True)
+vsm_ht = hl.read_table('gs://trisha-tmp/new_VSM_temp/vsm_base_em_gene_k_lag.ht')
 
-# other_columns = ['aa_pos', 'aa_ref', 'aa_alt', 'gene_symbol',
-#                 'ensp','uniprot_base', 'gene_id_em']
-# columns_to_specify = ['enst', 'mane_NM', 'mane_select', 'canonical', 'uniprot_em_id',  'transcript_mane_select']
-# vsm_ht = vsm_ht.collect_by_key()              
-# for c in other_columns:
-#     vsm_ht = vsm_ht.annotate(
-#         **{f'{c}':  hl.find(lambda x: hl.is_defined(x[c]) == True, vsm_ht.values)[c]}
-#     )
-# vsm_ht = vsm_ht.annotate(
-#     mane_enst = hl.find(lambda x: x.mane_select == True, vsm_ht.values)['enst'], 
-#     canon_enst = hl.find(lambda x: x.canonical == True, vsm_ht.values)['enst'], 
-#     **{'all_enst':  hl.filter(lambda x: hl.is_defined(x.enst) == True, vsm_ht.values).map(lambda x: x['enst'])},
+other_columns = ['aa_pos', 'aa_ref', 'aa_alt', 'gene_symbol',
+                'ensp','uniprot_base', 'gene_id_em']
+columns_to_specify = ['enst', 'mane_NM', 'mane_select', 'canonical', 'uniprot_em_id',  'transcript_mane_select']
+vsm_ht = vsm_ht.collect_by_key()              
+for c in other_columns:
+    vsm_ht = vsm_ht.annotate(
+        **{f'{c}':  hl.find(lambda x: hl.is_defined(x[c]) == True, vsm_ht.values)[c]}
+    )
+vsm_ht = vsm_ht.annotate(
+    mane_enst = hl.find(lambda x: x.mane_select == True, vsm_ht.values)['enst'], 
+    canon_enst = hl.find(lambda x: x.canonical == True, vsm_ht.values)['enst'], 
+    **{'all_enst':  hl.filter(lambda x: hl.is_defined(x.enst) == True, vsm_ht.values).map(lambda x: x['enst'])},
 
-#     mane_NM = hl.find(lambda x: x.mane_select == True, vsm_ht.values)['mane_NM'], 
-#     mane_select = hl.if_else(hl.is_defined(hl.find(lambda x: x.mane_select == True, vsm_ht.values)['mane_select']), True, False),
-#     canonical = hl.if_else(hl.is_defined(hl.find(lambda x: x.canonical == True, vsm_ht.values)['canonical']), True, False),
+    mane_NM = hl.find(lambda x: x.mane_select == True, vsm_ht.values)['mane_NM'], 
+    mane_select = hl.if_else(hl.is_defined(hl.find(lambda x: x.mane_select == True, vsm_ht.values)['mane_select']), True, False),
+    canonical = hl.if_else(hl.is_defined(hl.find(lambda x: x.canonical == True, vsm_ht.values)['canonical']), True, False),
 
-#     mane_uniprot_iso_em = hl.find(lambda x: x.mane_select == True, vsm_ht.values)['uniprot_em_id'],
-#     canon_uniprot_iso_em = hl.find(lambda x: x.canonical == True, vsm_ht.values)['uniprot_em_id'],
-#     **{'all_uniprot_iso_em':  hl.filter(lambda x: hl.is_defined(x.uniprot_em_id) == True, vsm_ht.values).map(lambda x: x['uniprot_em_id'])},
+    mane_uniprot_iso_em = hl.find(lambda x: x.mane_select == True, vsm_ht.values)['uniprot_em_id'],
+    canon_uniprot_iso_em = hl.find(lambda x: x.canonical == True, vsm_ht.values)['uniprot_em_id'],
+    **{'all_uniprot_iso_em':  hl.filter(lambda x: hl.is_defined(x.uniprot_em_id) == True, vsm_ht.values).map(lambda x: x['uniprot_em_id'])},
 
-#     transcript_mane_select = hl.find(lambda x: x.transcript_mane_select == True, vsm_ht.values)['transcript_mane_select'],
+    transcript_mane_select = hl.find(lambda x: x.transcript_mane_select == True, vsm_ht.values)['transcript_mane_select'],
 
-# )
-# vsm_ht = vsm_ht.checkpoint('gs://trisha-tmp/new_VSM_temp/vsm_base_per_gene_SNP.ht', overwrite=True)
-# vsm_ht = vsm_ht.annotate(
-#     mane_or_canon_enst = hl.coalesce(vsm_ht.mane_enst, vsm_ht.canon_enst, hl.is_missing(hl.tstr))
-# )
-# vsm_ht = vsm_ht.rename({'gene_id_em': 'gene_symbol_em'})
-# vsm_ht = vsm_ht.checkpoint('gs://trisha-tmp/new_VSM_temp/vsm_base_per_gene_SNP_2.ht', overwrite=True)
+)
+vsm_ht = vsm_ht.checkpoint('gs://trisha-tmp/new_VSM_temp/vsm_base_per_gene_SNP.ht', overwrite=True)
+vsm_ht = vsm_ht.annotate(
+    mane_or_canon_enst = hl.coalesce(vsm_ht.mane_enst, vsm_ht.canon_enst, hl.is_missing(hl.tstr))
+)
+vsm_ht = vsm_ht.rename({'gene_id_em': 'gene_symbol_em'})
+vsm_ht = vsm_ht.checkpoint('gs://trisha-tmp/new_VSM_temp/vsm_base_per_gene_SNP_2.ht', overwrite=True)
 
-# vsm_ht = hl.read_table('gs://trisha-tmp/new_VSM_temp/vsm_base_per_gene_SNP_2.ht')
-# vsm_ht = vsm_ht.drop('values')
-# vsm_ht = vsm_ht.checkpoint('gs://trisha-tmp/new_VSM_temp/vsm_base_per_gene_SNP_3.ht')
+vsm_ht = hl.read_table('gs://trisha-tmp/new_VSM_temp/vsm_base_per_gene_SNP_2.ht')
+vsm_ht = vsm_ht.drop('values')
+vsm_ht = vsm_ht.checkpoint('gs://trisha-tmp/new_VSM_temp/vsm_base_per_gene_SNP_3.ht')
 
 # 1.ENST based tables - join on locus, allele, enst
-# x = 0
-# final_str = ''
-# for method, info in joined_paths.items(): 
-#     print(method)
-#     if method == 'popeve': 
-#         continue
-#     ht = hl.read_table(f'gs://trisha-tmp/new_VSM_temp/per_SNP_gene/max_per_SNP_gene_{method}.ht')
-#     final_score_names = []
-#     for s in info['score_cols']:
-#         if info['higher_is_less_deleterious'][s]:
-#             final_score_names.append(f'{s}_neg')
-#         else:
-#             final_score_names.append(s)
-#     ht = ht.select(*final_score_names)
-#     vsm_ht = vsm_ht.join(ht, how='left')
-#     vsm_ht = vsm_ht.checkpoint(f'gs://trisha-tmp/new_VSM_temp/{x}_table_max_per_SNP_gene_{method}.ht', overwrite=True)
-#     final_str = f'gs://trisha-tmp/new_VSM_temp/{x}_table_max_per_SNP_gene_{method}.ht'
-#     x += 1
+x = 0
+final_str = ''
+for method, info in joined_paths.items(): 
+    print(method)
+    if method == 'popeve': 
+        continue
+    ht = hl.read_table(f'gs://trisha-tmp/new_VSM_temp/per_SNP_gene/max_per_SNP_gene_{method}.ht')
+    final_score_names = []
+    for s in info['score_cols']:
+        if info['higher_is_less_deleterious'][s]:
+            final_score_names.append(f'{s}_neg')
+        else:
+            final_score_names.append(s)
+    ht = ht.select(*final_score_names)
+    vsm_ht = vsm_ht.join(ht, how='left')
+    vsm_ht = vsm_ht.checkpoint(f'gs://trisha-tmp/new_VSM_temp/{x}_table_max_per_SNP_gene_{method}.ht', overwrite=True)
+    final_str = f'gs://trisha-tmp/new_VSM_temp/{x}_table_max_per_SNP_gene_{method}.ht'
+    x += 1
 
 # print('done: ', x)
 # print(final_str)
@@ -190,7 +190,6 @@ x += 1
 print(x)
 
 # 3. NO transcript tables - join on locus, alleles
-# vsm_ht = hl.read_table(f'gs://trisha-tmp/new_VSM_temp/table_max_per_SNP_gene_{method}_{x}.ht')
 vsm_ht = vsm_ht.key_by('locus', 'alleles')
 for method, info in no_transcript_tables.items(): 
     print(method)
@@ -210,44 +209,3 @@ for method, info in no_transcript_tables.items():
 vsm_ht = vsm_ht.checkpoint('gs://trisha-tmp/new_VSM_temp/vsm_all_tables/vsm_all_SNP_gene_la.ht', overwrite=True)
 vsm_ht = vsm_ht.key_by('locus', 'alleles', 'ensg')
 vsm_ht = vsm_ht.checkpoint('gs://trisha-tmp/new_VSM_temp/vsm_all_tables/vsm_all_SNP_gene_lag.ht', overwrite=True)
-
-# vsm_ht.show()
-
-# # Format
-# vsm_ht = hl.read_table('gs://trisha-tmp/new_VSM_temp/table_max_per_SNP_gene_all_k_lag.ht')
-# score_columns = ['proteinmpnn_llr_neg', 'esm1b_neg', 
-#             'score_PAI3D', 'revel', 'rasp_score', 'AM', 
-#             'MisFit_D', 'MisFit_S', 'polyphen_score', 'cpt1_score', 
-#             'popEVE_neg', 'EVE', 'ESM_1v_neg', 'cadd_score', 'gpn_msa_score']
-# other_columns = ['aa_pos', 'aa_ref', 'aa_alt', 'gene_symbol',
-#                 'ensp','uniprot_base', 'em_gene']
-# columns_to_specify = ['enst', 'mane_NM', 'mane_select', 'canonical', 'uniprot_em_id',  'transcript_mane_select']
-# vsm_ht = vsm_ht.collect_by_key()              
-# for s in score_columns:
-#     vsm_ht = vsm_ht.annotate(
-#         **{f'{s}':  hl.find(lambda x: hl.is_defined(x[s]) == True, vsm_ht.values)[s]}
-#     )
-# for c in other_columns:
-#     vsm_ht = vsm_ht.annotate(
-#         **{f'{c}':  hl.find(lambda x: hl.is_defined(x[c]) == True, vsm_ht.values)[c]}
-#     )
-# vsm_ht = vsm_ht.annotate(
-#     mane_enst = hl.find(lambda x: x.mane_select == True, vsm_ht.values)['enst'], 
-#     canon_enst = hl.find(lambda x: x.canonical == True, vsm_ht.values)['enst'], 
-#     **{'all_enst':  hl.filter(lambda x: hl.is_defined(x.enst) == True, vsm_ht.values).map(lambda x: x['enst'])},
-
-#     mane_NM = hl.find(lambda x: x.mane_select == True, vsm_ht.values)['mane_NM'], 
-#     mane_select = hl.if_else(hl.is_defined(hl.find(lambda x: x.mane_select == True, vsm_ht.values)['mane_select']), True, False),
-#     canonical = hl.if_else(hl.is_defined(hl.find(lambda x: x.canonical == True, vsm_ht.values)['canonical']), True, False),
-
-#     mane_uniprot_em_id = hl.find(lambda x: x.mane_select == True, vsm_ht.values)['uniprot_em_id'],
-#     canon_uniprot_em_id = hl.find(lambda x: x.canonical == True, vsm_ht.values)['uniprot_em_id'],
-#     **{'all_uniprot_em_id':  hl.filter(lambda x: hl.is_defined(x.uniprot_em_id) == True, vsm_ht.values).map(lambda x: x['uniprot_em_id'])},
-
-#     transcript_mane_select = hl.find(lambda x: x.transcript_mane_select == True, vsm_ht.values)['transcript_mane_select'],
-
-# )
-# vsm_ht = vsm_ht.checkpoint('gs://genetics-gym-not-public/Trisha/vsm_all_tables/vsm_all_SNP_gene.ht', overwrite=True)
-
-# vsm_ht = vsm_ht.drop('values')
-# vsm_ht.write('gs://genetics-gym-not-public/Trisha/vsm_all_tables/vsm_all_SNP_gene_cleaned.ht', overwrite=True)
